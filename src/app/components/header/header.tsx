e8116255f5a6951b6edf8e38e489681f1bb7f91a
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-10-05 19:08:10 -0300

    feat: add html structure and header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import './header.scss'
+
+export function Header(){
+  return (
+    <div className="header">
+    <div>
+      <h1>Hi, i´m Matheus! 👋</h1>
+      <h2>Software Engineer</h2>
+    </div>
+    <Image
+      src="/me.jpeg"
+      alt="My photo"
+      width={221}
+      height={225}
+      priority
+    />
+  </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Header } from './components/header/header';
+import './styles/home.scss'
 
 export default function Home() {
   return (
-    <main>
-       <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          width={100}
-          height={24}
-          priority
-        />
+    <main className='container'>
+      <Header/>
+      <div className="experience">
+        <h3>Experience</h3>
+        <p>Texto</p>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span>🇺🇸 EN - Basic</span>
+            <span>🇧🇷 PT-BR - Native Speaker</span>
+          </div>
+          <h3>Education</h3>
+          <div className="education-info">
+            <span>🎓</span>
+            <span>Onde estudei</span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social-icons">
+
+          </div>
+          <button>contact me!</button>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
