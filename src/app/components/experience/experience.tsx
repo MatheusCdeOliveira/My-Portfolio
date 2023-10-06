c73b48b1093f1e6d4aa7fd1f72d115ffb04ec541
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-10-06 15:29:30 -0300

    feat: experience section

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,14 @@
+import { SectionTitle } from "../sectionTitle/sectionTitle"
+import './experience.scss'
+
+export function Experience() {
+  return (
+    <div className="experience">
+    <SectionTitle text="Experience"/>
+    <p>Experience here.</p>
+    <div className="experience-time">
+
+    </div>
+    </div>
+  )
+}
