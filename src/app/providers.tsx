8e3b8c3960643a465de27a167a96ec4307b42bc0
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-10-17 16:45:13 -0300

    chore: portfolio description

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,11 @@
 'use client'
-import { Props } from './interfaces/IProvider';
+import { ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
- 
- 
+
+interface Props {
+  children?: ReactNode
+}
+
 export default function Provider({ children }: Props) {
   return <ThemeProvider>{children}</ThemeProvider>
 }
